ef69fada5a2ded7574c0080bb08ab63bd67d745c
lsw6684 <lsw6684@hanmail.net>
2022-05-31 00:23:11 +0900

    rest api test

---
+++ src/main/java/com/service/ITSM/ItsmApplication.java
@@ -0,0 +1,13 @@
+package com.service.ITSM;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItsmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ItsmApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/service/ITSM/ItsmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.service.ITSM;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ItsmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
