76368082adc686b9b6ce25a1096040121dbc8cd5
lsw6684 <lsw6684@hanmail.net>
2022-06-13 00:16:13 +0900

    수정 와꾸 완료~

--- src/main/java/com/service/ITSM/controller/BoardController.java
+++ src/main/java/com/service/ITSM/controller/BoardController.java
@@ -6,10 +6,7 @@ import com.service.ITSM.repository.BoardRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -28,8 +25,14 @@ public class BoardController {
     }
 
     @GetMapping("/form")
-    public String form(Model model) {
-        model.addAttribute("board", new Board());
+    public String form(Model model, @RequestParam(required = false) Long ID) {  // required : 필수인지 아닌지
+
+        if(ID == null)
+            model.addAttribute("board", new Board());
+        else {
+            Board board = boardRepository.findById(ID).orElse(null);
+            model.addAttribute("board", board);
+        }
         return "board/form";
     }
 
